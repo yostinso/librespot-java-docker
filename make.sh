55bcac9f83de03ad4813366d06230cf56cbb2306
E.O. Stinson <yostinso@aradine.com>
2023-11-24 15:02:16 -0800

    Working on build actions

--- make.sh
+++ make.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-LIBRESPOT_VERSION=1.6.3
-
 if [[ ! -f librespot-player-${LIBRESPOT_VERSION}.jar ]]; then
 	curl -O -L https://github.com/librespot-org/librespot-java/releases/download/v${LIBRESPOT_VERSION}/librespot-player-${LIBRESPOT_VERSION}.jar
 fi
